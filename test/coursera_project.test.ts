860ac5bac4519d1c74b2fc1fed36ef13951c7021
Furkan Perçin <furkanpercin@hotmail.com>
2022-07-23 11:40:55 +0300

    Initial commit

---
+++ test/coursera_project.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CourseraProject from '../lib/coursera_project-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/coursera_project-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CourseraProject.CourseraProjectStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
